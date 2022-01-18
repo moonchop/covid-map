b4b71e3d4cf5a9c94263e3062d56a203abe3d1e9
moonative <minssu913@naver.com>
2022-01-18 20:52:14 +0900

    add distance & infected

--- src/CovidMap.js
+++ src/CovidMap.js
@@ -32,7 +32,7 @@ function CovidMap() {
       try {
         const response = await axios.post("http://localhost:5000/covidData");
         setLocal(response.data)
-        console.log(response.data.updated_data)
+
       } catch {
         console.log("Error");
       }
@@ -40,24 +40,22 @@ function CovidMap() {
     fetchData();
   }, []);
 
-  useEffect(()=>{
-    console.log(clickLocal)
-    
-  },[clickLocal])
-
   const click=(id)=>{
     setClickLocal(id)
-    console.log(local.data[id].num)
+    setLocalData({
+      distance:local.data[id].level,
+      infected:local.data[id].num
+    })
   }
-  // if (local == null) {
-  //   return <p>Loading ...</p>;
-  // }
+  if (local == null) {
+    return <p>Loading ...</p>;
+  }
   return (
     <div>
       <h1>대한민국 지역별 코로나 현황</h1>
-      <h2>{clickLocal} 코로나 정보 </h2>
-      <div>거리두기 단계 : </div>
-      <div>확진자 수 : </div>
+      <h2>{clickLocal} 코로나 정보 ({local.updated_data}시 기준)</h2>
+      <div>거리두기 : {localData.distance}단계</div>
+      <div>확진자 수 : {localData.infected}명</div>
 
       <svg width="700px" height="1000px" viewBox="0 0 800 1200">
         <Seoul onClick={(e) => click(e.target.id)} />
